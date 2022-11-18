9e2a1987c6d76305cd0df3b5b31ffca2cdcf8a27
atalaycavusoglu <atalaycavusoglu1@gmail.com>
2022-11-18 16:03:16 +0300

    Initial Commit

---
+++ 4islemHesapMakinesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4islemHesapMakinesi
+//
+//  Created by Atalay Çavuşoğlu on 18.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
