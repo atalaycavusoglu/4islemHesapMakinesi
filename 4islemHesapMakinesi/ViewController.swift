f842a0a9a1a4bce7924cd53aa62de6afe844f96c
atalaycavusoglu <atalaycavusoglu1@gmail.com>
2022-11-18 22:27:30 +0300

    first commit

--- 4islemHesapMakinesi/ViewController.swift
+++ 4islemHesapMakinesi/ViewController.swift
@@ -9,11 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var birinciText: UITextField!
+    @IBOutlet weak var ikinciText: UITextField!
+    @IBOutlet weak var sonucLabel: UILabel!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    @IBAction func toplamaTiklandi(_ sender: Any) {
+        if let birinciSayi = Double(birinciText.text!) {
+            if let ikinciSayi = Double(ikinciText.text!) {
+                sonucLabel.text = String(birinciSayi + ikinciSayi)
+            }
+        }
+        
+    }
+    
+    
+    
+    @IBAction func cikarmaTiklandi(_ sender: Any) {
+        if let birinciSayi = Double(birinciText.text!) {
+            if let ikinciSayi = Double(ikinciText.text!) {
+                sonucLabel.text = String(birinciSayi - ikinciSayi)
+            }
+        }
+    }
+    
+    
+    
+    
+    @IBAction func carpmaTiklandi(_ sender: Any) {
+        if let birinciSayi = Double(birinciText.text!) {
+            if let ikinciSayi = Double(ikinciText.text!) {
+                sonucLabel.text = String(birinciSayi * ikinciSayi)
+            }
+        }
+    }
+    
+    
+    
+    
+    @IBAction func bolmeTiklandi(_ sender: Any) {
+        if let birinciSayi = Double(birinciText.text!) {
+            if let ikinciSayi = Double(ikinciText.text!) {
+                sonucLabel.text = String(birinciSayi / ikinciSayi)
+            }
+        }
+    }
+    
 }
 
